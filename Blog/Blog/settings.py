58ac3e23caf50d9e3f26db3cb88dc579cda38f04
Bnanyingi <58061814+Bnanyingi@users.noreply.github.com>
2021-07-16 23:31:38 +0300

    djangoupdate

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'theblog',
+    'theblog.apps.TheblogConfig',
 ]
 
 MIDDLEWARE = [
